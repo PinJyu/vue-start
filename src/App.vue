0169a2defc30bdd86967a986ac1804c1c8501ff8
yujianping <yujianping@sinosoft.hk>
2025-02-05 17:16:22 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="mx-auto max-w-7xl px-4 py-4 sm:px-6 sm:py-4 lg:px-8 bg-white z-50 sticky top-0">
+    <nav class="grid grid-cols-1 gap-x-4 sm:grid-cols-12 text-center">
+      <RouterLink to="/" class="rounded hover:bg-blue-300 duration-300">Home</RouterLink>
+      <div class="group block relative rounded hover:bg-blue-300 duration-300">
+        <a class="block">Tool</a>
+        <div
+          class="group-hover:scale-y-100 scale-y-0 duration-300 origin-top absolute bg-white divide-y divide-gray rounded border-[#cdcdcd] shadow-[0_1px_6px_#2b2a331a] text-left p-1 min-w-fit"
+        >
+          <RouterLink to="/tool1" class="block pl-4 pr-1 py-1"
+            >Tool1fadfafdasdfarfsdfasdfa</RouterLink
+          >
+          <RouterLink to="/tool1" class="block pl-4 pr-1 py-1">Tool2</RouterLink>
+          <RouterLink to="/tool1" class="block pl-4 pr-1 py-1">Tool2</RouterLink>
+          <RouterLink to="/tool1" class="block pl-4 pr-1 py-1">Tool2</RouterLink>
+          <RouterLink to="/tool1" class="block pl-4 pr-1 py-1">Tool2</RouterLink>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+/* a {
+  display: inline-block;
+} */
+</style>
